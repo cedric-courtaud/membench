55fc49dfe202f74a7640046349829e578f4bcf8f
Cédric Courtaud <cedric.courtaud@gmail.com>
2020-05-30 10:24:30 +0200

    includes <memory> header

--- cmd/include/instances.h
+++ cmd/include/instances.h
@@ -6,6 +6,7 @@
 #define MEMBENCH_INSTANCES_H
 
 #include <map>
+#include <memory>
 #include <membench/MemBenchParams.h>
 
 extern std::map<const std::string, std::shared_ptr<membench::MemBenchInstance> > instances;
